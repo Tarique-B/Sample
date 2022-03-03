06a2a04db25154fcdfcbcda64146fd52fecdc488
tarique <tarique@gmail.com>
2022-03-03 10:41:23 +0000

    update2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,13 @@
 package com.mycompany.app;
 
 /**
- * Hello world, this is tarique!
+ * Hello world!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" )
     }
 }
